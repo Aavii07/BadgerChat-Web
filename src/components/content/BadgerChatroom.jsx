80c890b480b366276a85bb4b023a6419bc0e6ef4
Avi Shukla <avi.l.shukla@gmail.com>
2024-07-06 15:19:10 -0700

    revisited code to fix pagination for real this time and streamline code (turns out pagination is handled in the get messages api request, so I was paginating already paginated data)

--- src/components/content/BadgerChatroom.jsx
+++ src/components/content/BadgerChatroom.jsx
@@ -6,40 +6,31 @@ export default function BadgerChatroom(props) {
 
     const [messages, setMessages] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const [displayedPosts, setDisplayedPosts] = useState([]);
 
     const totalPages = [1, 2, 3, 4]; // can be hardcoded
 
     const storedLoginStatus = JSON.parse(sessionStorage.getItem("loginStatus"));
-    const [loginStatus, setLoginStatus] = useState(storedLoginStatus || false)
+    const [loginStatus] = useState(storedLoginStatus || false);
     const loggedInUsername = sessionStorage.getItem("username");
 
-
-    const loadMessages = () => {
-        fetch(`https://cs571api.cs.wisc.edu/rest/su24/hw6/messages?chatroom=${props.name}`, {
+    const loadMessages = (pageNumber) => {
+        fetch(`https://cs571api.cs.wisc.edu/rest/su24/hw6/messages?chatroom=${props.name}&page=${pageNumber}`, {
             headers: {
                 "X-CS571-ID": CS571.getBadgerId()
             }
         }).then(res => res.json()).then(json => {
-            setMessages(json.messages)
-        })
+            setMessages(json.messages);
+        });
     };
 
     // reload messages for chatroom or page change
     useEffect(() => {
-        loadMessages();
-    }, [currentPage]);
-
-    // update messages display
-    useEffect(() => {
-        const startIndex = (currentPage - 1) * 25;
-        const endIndex = currentPage * 25;
-        setDisplayedPosts(messages.slice(startIndex, endIndex));
-    }, [messages]);
-
+        loadMessages(currentPage);
+    }, [currentPage, props]);
 
     const handlePageChange = (pageNumber) => {
         setCurrentPage(pageNumber);
+        console.log(messages);
     };
 
     const handlePostSubmit = (e) => {
@@ -48,13 +39,13 @@ export default function BadgerChatroom(props) {
         const title = e.target.elements.postTitle.value;
         const content = e.target.elements.postContents.value;
 
-        // pre api check
+        // pre API check
         if (!title || !content) {
             alert("You must provide both a title and content!");
             return;
         }
 
-        // api call
+        // API call
         fetch(`https://cs571api.cs.wisc.edu/rest/su24/hw6/messages?chatroom=${props.name}`, {
             method: 'POST',
             credentials: 'include',
@@ -69,25 +60,17 @@ export default function BadgerChatroom(props) {
         })
         .then(res => {
             if (res.status === 200) {
-                return res.json();
+                alert("Successfully posted!");
+                loadMessages(currentPage);
             } else {
-                alert("Error, message could not be posted");
-                return Promise.reject("cannot post message"); // skip .then and go to catch
+                alert("Error, message could not be posted. Be careful not to make the title or contents extremely massive.");
+                return ("cannot post message");
             }
-        })
-        .then(json => {
-            alert("Successfully posted!");
-            loadMessages();
         })
         .catch(error => {
             console.error("Error:", error);
-        })
-    }
-
-    // Why can't we just say []?
-    // The BadgerChatroom doesn't unload/reload when switching
-    // chatrooms, only its props change! Try it yourself.
-    useEffect(loadMessages, [props]);
+        });
+    };
 
     return <>
         <h1>{props.name} Chatroom</h1>
@@ -110,16 +93,16 @@ export default function BadgerChatroom(props) {
             )}
         <hr/>
         {
-            displayedPosts.length > 0 ?
+            messages.length > 0 ?
                 <>
                     {
                         <Container>
                             <Row> {
-                                displayedPosts.map((post) => (   
+                                messages.map((post) => (   
                                     <Col key={post.id} xs={12} sm={6} md={6} lg={4} xl={3}>
                                         <BadgerMessage {...post} 
                                             loggedInUsername={loggedInUsername} // used to check if you made the post
-                                            loadMessages={loadMessages} // allows delete function in badgerMessage to reload messages
+                                            loadMessages={() => loadMessages(currentPage)} // allows delete function in badgerMessage to reload messages
                                         />
                                     </Col>
                                     )) }
