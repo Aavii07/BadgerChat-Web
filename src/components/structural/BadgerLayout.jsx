adbb86e29c3d011f293428545a7abc037287dad7
Avi Shukla <avi.l.shukla@gmail.com>
2024-07-23 00:53:36 -0700

    fixed layout of navBar by removing extra container and utilziing navbar>toggle and Collapse

--- src/components/structural/BadgerLayout.jsx
+++ src/components/structural/BadgerLayout.jsx
@@ -1,26 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
-import crest from '../../assets/uw-crest.svg'
+import crest from '../../assets/uw-crest.svg';
 import BadgerLoginStatusContext from "../contexts/BadgerLoginStatusContext";
 
 function BadgerLayout(props) {
-
-    // You'll probably want to see if there is an existing
-    // user in sessionStorage first. If so, that should
-    // be your initial loginStatus state.
     const storedLoginStatus = JSON.parse(sessionStorage.getItem("loginStatus"));
-    const [loginStatus, setLoginStatus] = useState(storedLoginStatus || false)
+    const [loginStatus, setLoginStatus] = useState(storedLoginStatus || false);
 
-    const handleLogout = () => { // onClick in arrow function stops it from being called immediately on render
+    const handleLogout = () => {
         setLoginStatus(false);
-        sessionStorage.removeItem("loginStatus"); 
+        sessionStorage.removeItem("loginStatus");
         alert("You have been logged out!");
-    }
+    };
 
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" variant="dark" expand="lg">
                 <Container>
                     <Navbar.Brand as={Link} to="/">
                         <img
@@ -32,34 +28,29 @@ function BadgerLayout(props) {
                         />{' '}
                         BadgerChat
                     </Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
-
-                        {!loginStatus ? 
-                        <Container>
-                            <Nav className="me-auto">  
-                                <Nav.Link as={Link} to="login">Login</Nav.Link>
-                                <Nav.Link as={Link} to="register">Register</Nav.Link>
-                            </Nav>
-                        </Container> : 
-                        <Container>
-                            <Nav.Link 
-                                as={Link} to="logout" 
-                                onClick={handleLogout}> 
-                                Logout
-                            </Nav.Link>
-                        </Container>}
-                        
-                        <NavDropdown title="Chatrooms">
-                            {
-                                props.chatrooms.map(chatroom => (
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            {!loginStatus ? (
+                                <>
+                                    <Nav.Link as={Link} to="login">Login</Nav.Link>
+                                    <Nav.Link as={Link} to="register">Register</Nav.Link>
+                                </>
+                            ) : (
+                                <Nav.Link as={Link} to="logout" onClick={handleLogout}>
+                                    Logout
+                                </Nav.Link>
+                            )}
+                            <NavDropdown title="Chatrooms">
+                                {props.chatrooms.map(chatroom => (
                                     <NavDropdown.Item key={chatroom} as={Link} to={`/chatrooms/${chatroom}`}>
                                         {chatroom}
                                     </NavDropdown.Item>
-                                ))
-                            }
-                        </NavDropdown>
-                    </Nav>
+                                ))}
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
             <div style={{ margin: "1rem" }}>
